caea824b62eb9f9534e442b7627e93995db02080
Raphael Freitas <raphaeldefreitas2009@gmail.com>
2019-09-01 15:48:36 -0300

    Atualização do dashboard com logo personalizado

--- Code/Microcontroller/ESP32_GADAO/src/main.cpp
+++ Code/Microcontroller/ESP32_GADAO/src/main.cpp
@@ -1,44 +1,57 @@
-#define WIFI_MODO STA
+#define WIFI_MODE_CONNECT
 
 #include <Arduino.h>
-#include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <ESPDash.h>
+#include <WiFi.h>
 
 AsyncWebServer server(80);
 
+#if defined WIFI_MODE_SERVE
 const char *ssid = "TT-Gadao";
 const char *password = "guerreiro";
+#elif defined WIFI_MODE_CONNECT
+const char *ssid = "VIVOFIBRA-5C6C";
+const char *password = "c662005c6c";
+#endif
 
 int indicator = 2;
 
 bool LED = LOW;
 
-void buttonClicked(const char *id)
-{
-    LED = !LED;
-    digitalWrite(indicator, LED);
+void buttonClicked(const char *id) {
+  LED = !LED;
+  digitalWrite(indicator, LED);
 }
 
-void setup()
-{
+void setup() {
 
-    Serial.begin(9600);
-    pinMode(indicator, OUTPUT);
-     WiFi.mode(WIFI_AP);
-    WiFi.softAP(ssid, password);
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.softAPIP());
+  Serial.begin(9600);
+  pinMode(indicator, OUTPUT);
 
-    ESPDash.init(server);
+#if defined WIFI_MODE_SERVE
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.softAPIP());
 
-    ESPDash.addButtonCard("btn1", "Botão LED");
-    ESPDash.attachButtonClick(buttonClicked);
+#elif defined WIFI_MODE_CONNECT
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.printf("WiFi Failed!\n");
+    return;
+  }
 
-    server.begin();
+#endif
+
+  ESPDash.init(server);
+
+  ESPDash.addButtonCard("btn1", "Botão LED");
+  ESPDash.attachButtonClick(buttonClicked);
+
+  server.begin();
 }
 
-void loop()
-{
-}
+void loop() {}
