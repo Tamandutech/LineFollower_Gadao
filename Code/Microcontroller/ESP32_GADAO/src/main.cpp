77561fb86c4e7651e639a351754605ae29d00b56
Raphael Freitas <raphaeldefreitas2009@gmail.com>
2019-11-13 01:34:37 -0300

    Teste MCP3008 + Array

--- Code/Microcontroller/ESP32_GADAO/src/main.cpp
+++ Code/Microcontroller/ESP32_GADAO/src/main.cpp
@@ -1,31 +1,33 @@
-#define WIFI_MODE_SERVE
+#define WIFI_MODE_CONNECT
 
+#include <Adafruit_MCP3008.h>
 #include <Arduino.h>
-#include <Hash.h>
+#include <AsyncOTA.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <ESPDash.h>
-#include <WiFi.h>
-#include <AsyncOTA.h>
+#include <Hash.h>
 #include <QTRSensors.h>
+#include <WiFi.h>
 
-#define NUM_SENSORS             6  // number of sensors used
-#define NUM_SAMPLES_PER_SENSOR  4  // average 4 analog samples per sensor reading
-#define EMITTER_PIN             4  // emitter is controlled by digital pin 2
+#define NUM_SENSORS 6            // number of sensors used
+#define NUM_SAMPLES_PER_SENSOR 4 // average 4 analog samples per sensor reading
+#define EMITTER_PIN 4            // emitter is controlled by digital pin 2
 
 // sensors 0 through 5 are connected to analog inputs 0 through 5, respectively
-QTRSensorsAnalog qtra((unsigned char[]) {36, 39, 34, 35, 32, 33},
-NUM_SENSORS, NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
+QTRSensorsAnalog qtra((unsigned char[]){36, 39, 34, 35, 32, 33}, NUM_SENSORS,
+                      NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
 unsigned int sensorValues[NUM_SENSORS];
 
 AsyncWebServer server(80);
+Adafruit_MCP3008 adc;
 
 #if defined WIFI_MODE_SERVE
 const char *ssid = "TT-Gadao";
 const char *password = "guerreiro";
 #elif defined WIFI_MODE_CONNECT
-const char *ssid = "AndroidAP000C";
-const char *password = "guerreiro";
+const char *ssid = "RFREITAS";
+const char *password = "941138872";
 #endif
 
 int count = 0;
@@ -40,7 +42,7 @@ int count = 0;
 #define LEDC_BASE_FREQ 5000
 
 // fade LED PIN (replace with LED_BUILTIN constant for built-in LED)
-#define LED_PIN 3
+#define LED_PIN LED_BUILTIN
 
 int brightness = 0; // how bright the LED i
 
@@ -48,8 +50,7 @@ int indicator = 2;
 
 bool LED = LOW;
 
-void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 100)
-{
+void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 100) {
   // calculate duty, 8191 from 2 ^ 13 - 1
   uint32_t duty = (8191 / valueMax) * min(value, valueMax);
 
@@ -57,149 +58,73 @@ void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 100)
   ledcWrite(channel, duty);
 }
 
-void buttonClicked(const char *id)
-{
-  LED = !LED;
-  digitalWrite(indicator, LED);
-}
-
-void sliderAlterado(const char *id, const int sliderValue)
-{
+void sliderAlterado(const char *id, const int sliderValue) {
   ledcAnalogWrite(LEDC_CHANNEL_0, sliderValue);
 }
 
-void IRAM_ATTR isr() {
-  count++;
-}
-
-
-void setup()
-{
+void setup() {
 
   // Setup timer and attach timer to a led pin
   ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
   ledcAttachPin(LED_PIN, LEDC_CHANNEL_0);
-
-  pinMode(21, INPUT_PULLUP);
-  attachInterrupt(21, isr, FALLING);
-
-  pinMode(19, INPUT_PULLUP);
-  attachInterrupt(19, isr, FALLING);
 
   Serial.begin(9600);
 
 #if defined WIFI_MODE_SERVE
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ssid, password);
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.softAPIP());
-
 #elif defined WIFI_MODE_CONNECT
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED)
-  {
-    Serial.printf("WiFi Failed!\n");
-    return;
-  }
-
 #endif
 
-
+  adc.begin();
 
   ESPDash.init(server);
 
-  //ESPDash.addButtonCard("btn1", "Botão LED");
-  //ESPDash.attachButtonClick(buttonClicked);
+  // ESPDash.addButtonCard("btn1", "Botão LED");
+  // ESPDash.attachButtonClick(buttonClicked);
 
   ESPDash.addSliderCard("slider1", "Slider PWM", 2);
   ESPDash.attachSliderChanged(sliderAlterado);
 
-  ESPDash.addNumberCard("num1", "Encoder 1", 0);
+  ESPDash.addNumberCard("num1", "Sensor 1", 0);
+  ESPDash.addNumberCard("num2", "Sensor 2", 0);
+  ESPDash.addNumberCard("num3", "Sensor 3", 0);
+  ESPDash.addNumberCard("num4", "Sensor 4", 0);
+  ESPDash.addNumberCard("num5", "Sensor 5", 0);
+  ESPDash.addNumberCard("num6", "Sensor 6", 0);
+  ESPDash.addNumberCard("num7", "Sensor 7", 0);
+  ESPDash.addNumberCard("num8", "Sensor 8", 0);
 
-  //OTA
-  AsyncOTA.begin(&server);    // Inicia o servidor de OTA
+  // OTA
+  AsyncOTA.begin(&server); // Inicia o servidor de OTA
 
   server.begin();
+  adc.begin(22);
 
-    delay(500);
+  delay(500);
   pinMode(23, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);    // turn on Arduino's LED to indicate we are in calibration mode
-  for (int i = 0; i < 2000; i++)  // make the calibration take about 10 seconds
-  {
-    qtra.calibrate();       // reads all sensors 10 times at 2.5 ms per six sensors (i.e. ~25 ms per call)
-  }
-  digitalWrite(LED_BUILTIN, LOW);     // turn off Arduino's LED to indicate we are through with calibration
 
-  // print the calibration minimum values measured when emitters were on
-
-  for (int i = 0; i < NUM_SENSORS; i++)
-  {
-    Serial.print(qtra.calibratedMinimumOn[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-
-  // print the calibration maximum values measured when emitters were on
-  for (int i = 0; i < NUM_SENSORS; i++)
-  {
-    Serial.print(qtra.calibratedMaximumOn[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  Serial.println();
-  delay(1000);
-
-  Serial.println("Terminou o setup!");
-}
-
-void loop()
-{
-
-  // loop do OTA
-  AsyncOTA.loop();
-
-  /*  // read calibrated sensor values and obtain a measure of the line position from 0 to 5000
-  // To get raw sensor values, call:
-  //  qtra.read(sensorValues); instead of unsigned int position = qtra.readLine(sensorValues);
-  unsigned int position = qtra.readLine(sensorValues);
-
-  // print the sensor values as numbers from 0 to 1000, where 0 means maximum reflectance and
-  // 1000 means minimum reflectance, followed by the line position
-  for (unsigned char i = 0; i < NUM_SENSORS; i++)
-  {
-    Serial.print(sensorValues[i]);
-    
-    Serial.print('\t');
-  }
-
-  Serial.print(digitalRead(23));
-
-  Serial.print('\t');
-
-  //Serial.println(); // uncomment this line if you are using raw values
-  Serial.println(position); // comment this line out if you are using raw values
-
-  //delay(250);
- */
-  ESPDash.updateNumberCard("num1", count);
-  delay(250);
-}
-/* 
-#include <Arduino.h>
-#define sensor 4
-
-int i = 0;
-
-void setup() {
-  pinMode(sensor, INPUT);
-  Serial.begin(9600);
 }
 
 void loop() {
-  i++;
-  int s1 = digitalRead(sensor);
-  Serial.printf("Leitura %d: ", i);
-  Serial.println(s1);
-} */
+  AsyncOTA.loop();
+  ESPDash.updateNumberCard("num1", adc.readADC(0));
+  delay(100);
+  ESPDash.updateNumberCard("num2", adc.readADC(1));
+  delay(100);
+  ESPDash.updateNumberCard("num3", adc.readADC(2));
+  delay(100);
+  ESPDash.updateNumberCard("num4", adc.readADC(3));
+  delay(100);
+  ESPDash.updateNumberCard("num5", adc.readADC(4));
+  delay(100);
+  ESPDash.updateNumberCard("num6", adc.readADC(5));
+  delay(100);
+  ESPDash.updateNumberCard("num7", adc.readADC(6));
+  delay(100);
+  ESPDash.updateNumberCard("num8", adc.readADC(7));
+  delay(100);
+}
